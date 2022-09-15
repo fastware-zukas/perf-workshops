1c8e89b0897775283b8fc811ef4869f487c2f256
Julius Zukauskas <julius.zukauskas@gmx.com>
2022-09-15 01:21:12 +0100

    1.intro impl

--- 1.intro/main.cpp
+++ 1.intro/main.cpp
@@ -2,7 +2,6 @@
 #include <cstdlib>
 #include <random>
 
-
 class string_data_fixture : public ::benchmark::Fixture {
 public:
   void SetUp(const ::benchmark::State &st) {
@@ -42,7 +41,6 @@ public:
       ->Arg(9)                                                                 \
       ->Arg(10);
 
-
 // Baseline 1
 BENCHMARK_DEFINE_F(string_data_fixture, std_atoi)(benchmark::State &state) {
   for (auto _ : state) {
@@ -56,30 +54,218 @@ BENCHMARK_DEFINE_F(string_data_fixture, std_atoi)(benchmark::State &state) {
 
 BENCH(std_atoi);
 
-
-/** ----- Your implementation start here ----- */
-
-// Replace *my* in 'my_str_to_int' with your username to avoid dupticates
-static int my_str_to_int(const char *str)
-{
-  return 0;
+int ssedov_str_to_int(const char *str) {
+  if (str[0] == '-')
+    return -ssedov_str_to_int(str + 1);
+  int x = 0;
+  int i = 0;
+  while ((str[i] & 0xf0) == 0x30 && (str[i] < 0x3a))
+    x = x * 10 + str[i++] - 0x30;
+  return x;
 }
 
-// Replace *my* in 'my_sring_to_int' with your username to avoid dupticates
-BENCHMARK_DEFINE_F(string_data_fixture, my_sring_to_int)(benchmark::State &state) {
+BENCHMARK_DEFINE_F(string_data_fixture, ssedov_str_to_int)
+(benchmark::State &state) {
   for (auto _ : state) {
     for (const auto &[str, integer] : values) {
-      const auto val = my_str_to_int(str.c_str());
+      const auto val = ssedov_str_to_int(str.c_str());
       assert(val == integer);
       benchmark::DoNotOptimize(&val);
     }
   }
 }
 
-BENCH(my_sring_to_int);
+BENCH(ssedov_str_to_int);
 
+static int michael_str_to_int(const char *str) {
+  int val = 0;
+  short sign = 1;
+  size_t i = 0;
+  if (str[0] == '-') {
+    i++;
+    sign = -1;
+  }
+  for (; str[i] != '\0'; i++) {
+    val = (val << 3) + (val << 1) + (str[i] - '0');
+  }
+  return val * sign;
+}
 
-/** ----- Your implementation ends here ----- */
+BENCHMARK_DEFINE_F(string_data_fixture, michael_sring_to_int)
+(benchmark::State &state) {
+  for (auto _ : state) {
+    for (const auto &[str, integer] : values) {
+      const auto val = michael_str_to_int(str.c_str());
+      assert(val == integer);
+      benchmark::DoNotOptimize(&val);
+    }
+  }
+}
 
+BENCH(michael_sring_to_int);
+
+static int jlane71_str_to_int(const char *str, int length) {
+  int output = 0;
+  bool negative = false;
+
+  if (*str == '-') {
+    negative = true;
+    ++str;
+    --length;
+  }
+
+  switch (length) {
+  case 10:
+    output += 1000000000 * (str[length - 10] - '0');
+  case 9:
+    output += 100000000 * (str[length - 9] - '0');
+  case 8:
+    output += 10000000 * (str[length - 8] - '0');
+  case 7:
+    output += 1000000 * (str[length - 7] - '0');
+  case 6:
+    output += 100000 * (str[length - 6] - '0');
+  case 5:
+    output += 10000 * (str[length - 5] - '0');
+  case 4:
+    output += 1000 * (str[length - 4] - '0');
+  case 3:
+    output += 100 * (str[length - 3] - '0');
+  case 2:
+    output += 10 * (str[length - 2] - '0');
+  case 1:
+    output += (str[length - 1] - '0');
+  }
+
+  return negative ? -output : output;
+}
+
+// Replace *my* in 'my_sring_to_int' with your username to avoid dupticates
+BENCHMARK_DEFINE_F(string_data_fixture, jlane71_sring_to_int)
+(benchmark::State &state) {
+  for (auto _ : state) {
+    for (const auto &[str, integer] : values) {
+      const auto val = jlane71_str_to_int(str.c_str(), str.length());
+      assert(val == integer);
+      benchmark::DoNotOptimize(&val);
+    }
+  }
+}
+
+BENCH(jlane71_sring_to_int);
+
+static int yzhu631_str_to_int(const char *str) {
+  char const *ptr = str;
+  int rlt = 0;
+  int sign = 1;
+  if (*ptr == '-') {
+    sign = -1;
+    ptr++;
+  }
+  while (*ptr) {
+    rlt = *ptr - '0' + (rlt << 3) + (rlt << 1);
+    ptr++;
+  }
+  return sign * rlt;
+}
+
+BENCHMARK_DEFINE_F(string_data_fixture, yzhu631_str_to_int)
+(benchmark::State &state) {
+  for (auto _ : state) {
+    for (const auto &[str, integer] : values) {
+      const auto val = yzhu631_str_to_int(str.c_str());
+      assert(val == integer);
+      benchmark::DoNotOptimize(&val);
+    }
+  }
+}
+
+BENCH(yzhu631_str_to_int);
+
+static int mzambotti1_string_to_int(const char *first) {
+  int result{0};
+  int sign{1};
+  if (((*first == '-'))) {
+    sign = -1;
+    ++first;
+  }
+  for (; *first != '\0'; ++first) {
+    if ((!(('0' <= *first) && (*first <= '9'))))
+      throw std::exception();
+    result = result * 10 + unsigned(*first - '0');
+  }
+  return sign * result;
+}
+
+BENCHMARK_DEFINE_F(string_data_fixture, mzambotti1_string_to_int)
+(benchmark::State &state) {
+  for (auto _ : state) {
+    for (const auto &[str, integer] : values) {
+      const auto val = mzambotti1_string_to_int(str.c_str());
+      assert(val == integer);
+      benchmark::DoNotOptimize(&val);
+    }
+  }
+}
+
+BENCH(mzambotti1_string_to_int);
+
+static int tej_str_to_int(const char *str) {
+  int val = 0;
+  while (*str) {
+    val = val * 10 + (*str++ - '0');
+  }
+  return val;
+}
+
+// Replace *my* in 'my_sring_to_int' with your username to avoid dupticates
+BENCHMARK_DEFINE_F(string_data_fixture, tej_sring_to_int)
+(benchmark::State &state) {
+  for (auto _ : state) {
+    for (const auto &[str, integer] : values) {
+      const auto val = tej_str_to_int(str.c_str());
+      assert(val == integer);
+      benchmark::DoNotOptimize(&val);
+    }
+  }
+}
+
+BENCH(tej_sring_to_int);
+
+int jz_sring_to_int(const char *str) {
+  if (str == nullptr)
+    return 0;
+
+  int res = 0;  // Initialize result
+  int sign = 1; // Initialize sign as positive
+
+  // If number is negative, then update sign
+  if (*str == '-') {
+    sign = -1;
+    str++; // Also update index of first digit
+  }
+
+  // Iterate through all digits of input string and update result
+  for (uint8_t c = uint8_t(*str) - '0'; c <= 9; c = uint8_t(*++str) - '0') {
+    res = res * 10 + int(c);
+  }
+
+  // Return result with sign
+  return (*str == '\0' ? res * sign : 0);
+}
+
+BENCHMARK_DEFINE_F(string_data_fixture, jz_sring_to_int)
+(benchmark::State &state) {
+
+  for (auto _ : state) {
+    for (const auto &[str, integer] : values) {
+      const auto val = jz_sring_to_int(str.c_str());
+      assert(val == integer);
+      benchmark::DoNotOptimize(&val);
+    }
+  }
+}
+
+BENCH(jz_sring_to_int);
 
 BENCHMARK_MAIN();
